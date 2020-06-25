b41bc9a697c443d67750ee712786a9b8ed7a1732
Tom Herrmann <tomherrmanndev@gmail.com>
2020-06-25 15:25:31 -0400

    Got hot module refresh working

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    hotOnly: true,
+    hot: true,
     port: 8000,
     proxy: {
       '/': {
-        target: 'http://localhost:3000'
-      }
-    }
+        target: 'http://localhost:3000',
+      },
+    },
   },
   entry: ['babel-polyfill', './src/index.js'],
   mode: 'development',
@@ -20,30 +20,30 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/i,
-        loader: 'html-loader'
+        loader: 'html-loader',
       },
 
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
